2351ad9b77ad43c46a9779efb191e5fb1d6d4b94
mdmanik74 <manikmhamud74@outlook.com>
2023-04-18 17:37:56 +0600

    Alert Dialog system

---
+++ lib/Alertdialogs.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Alertdialogs extends StatelessWidget {
+  const Alertdialogs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Are Your Sure'),
+          content: const Text('This Item deleted !!!!!!!!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Show Dialog'),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,40 @@
 import 'package:flutter/material.dart';
+import 'Alertdialogs.dart';
+void main() =>runApp(MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      appBar: AppBar(title: Text("Alert Dialog Sample"),),
+      body: const Center(
+        child: Alertdialogs(),
+      ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
@@ -34,7 +70,7 @@ class MyApp extends StatelessWidget{
 
 
 
-
+*/
 
 
 
