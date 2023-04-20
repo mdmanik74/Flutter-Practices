2982af9aa86f8f9a050d6fae6acfb9e3eb16724a
mdmanik74 <manikmhamud74@outlook.com>
2023-04-20 16:55:06 +0600

    card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,42 @@
 import 'package:flutter/material.dart';
-class DataTableDemo extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Data Tables'),
-      ),
-
-
-    );
-  }
-}
-
-class MyStatelessWidget extends StatelessWidget{
-  const MyStatelessWidget ({super.key});
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return DataTable(
-    columns: [
-      DataColumn(label:Expanded(
-        child: Text(
-          "Name", style: TextStyle(fontSize: 30, fontStyle: FontStyle.italic),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Simple Alert Dialog'),
         ),
-      ),
-      ),
-      DataColumn(
-        label: Expanded(
-          child: Text(
-            'Role',
-            style: TextStyle(fontStyle: FontStyle.italic),
+        body: Center(
+          child: Card(
+            elevation: 50,
+            shadowColor: Colors.black,
+            color: Colors.greenAccent[100],
+            child: SizedBox(
+              width: 300,
+              height: 500,
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.green[500],
+                      radius: 108,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"
+                        ),
+                        radius: 100,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
-    ],
-      rows: const [
-        DataRow(
-          cells: [
-            DataCell(Text('Manik')),
-            DataCell(Text('19')),
-            DataCell(Text('Student')),
-          ]
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Janine')),
-            DataCell(Text('43')),
-            DataCell(Text('Professor')),
-          ],
-        ),
-      ],
     );
   }
 }
--- sourcess/sourcecode.dart
+++ sourcess/sourcecode.dart
@@ -1,4 +1,67 @@
 /*
+
+
+
+
+
+
+/*
+import 'package:flutter/material.dart';
+class DataTableDemo extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Tables'),
+      ),
+
+
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget{
+  const MyStatelessWidget ({super.key});
+  @override
+  Widget build(BuildContext context){
+    return DataTable(
+    columns: [
+      DataColumn(label:Expanded(
+        child: Text(
+          "Name", style: TextStyle(fontSize: 30, fontStyle: FontStyle.italic),
+        ),
+      ),
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Role',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ),
+    ],
+      rows: const [
+        DataRow(
+          cells: [
+            DataCell(Text('Manik')),
+            DataCell(Text('19')),
+            DataCell(Text('Student')),
+          ]
+        ),
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('Janine')),
+            DataCell(Text('43')),
+            DataCell(Text('Professor')),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+ */
 import 'package:flutter/material.dart';
 
 void main() =>runApp(MyApp());
