774b57028a2e9efb33ba5ddfe857e8075587c711
mdmanik74 <manikmhamud74@outlook.com>
2023-04-30 19:27:05 +0600

    Dialog box

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,66 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Simple Alert Dialog'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Custom AlertDialog Example'),
         ),
         body: Center(
-          child: Card(
-            elevation: 50,
-            shadowColor: Colors.black,
-            color: Colors.greenAccent[100],
-            child: SizedBox(
-              width: 300,
-              height: 500,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.green[500],
-                      radius: 108,
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"
-                        ),
-                        radius: 100,
+            child: TextButton(
+              onPressed: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => Dialog(
+                    elevation: 0,
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: SizedBox(
+                      height: 300,
+                      width: 400,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const FlutterLogo(
+                            size: 100,
+                          ),
+                          const Text(
+                            "Custom Dialog Example in Flutter",
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text("Okay"))
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+                    ),
+                  )),
+              child: const Text('Show Dialog'),
+            )));
   }
 }
