c42773a3c87ffce83ad6785c297bcd9a31263cc0
mdmanik74 <manikmhamud74@outlook.com>
2023-05-02 14:15:00 +0600

    button design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,48 @@
 import 'package:flutter/material.dart';
+void main () => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title= 'Flutter Code Sample';
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title),),
+        body: const MyStatelessWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget{
+  const MyStatelessWidget({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      color: Colors.white,
+      child: Center(
+        child: Ink(
+          decoration: const ShapeDecoration(
+            color: Colors.lightBlue,
+            shape: CircleBorder(),
+          ),
+          child: IconButton(
+            icon: Icon(Icons.dangerous),
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatelessWidget {
@@ -66,7 +110,7 @@ class HomePage extends StatelessWidget {
         ));
   }
 }
-
+*/
 
 /*
 import 'package:flutter/material.dart';
