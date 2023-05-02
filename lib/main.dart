0399ed0b2a9b0e06812e04cb30290b73bdeba6a9
mdmanik74 <manikmhamud74@outlook.com>
2023-05-02 12:18:44 +0600

    flutter dialog alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text("Flutter Cupertino Alert Dialog Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset("assets/logo.png",scale: 12,),
+            SizedBox(height: 50,),
+            RaisedButton(
+              color: Colors.green[100],
+              onPressed: () {
+                _showDialog(context);
+              },
+              child: Text('Show AlertDialog'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showDialog(BuildContext context){
+    showDialog(
+        context: context,
+        child:   CupertinoAlertDialog(
+          title: Column(
+            children: <Widget>[
+              Text("CupertinoAlertDialog"),
+              Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),
+            ],
+          ),
+          content: new Text( "An iOS-style alert dialog."+
+              "An alert dialog informs the user about situations that require acknowledgement."
+                  " An alert dialog has an optional title, optional content, and an optional list of actions."),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },),
+            CupertinoDialogAction(
+              child: Text("CANCEL"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },),
+            CupertinoDialogAction(
+              child: Text("MAY BE"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },),
+          ],
+        ));
+  }
+}
+
+
+/*
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -64,3 +135,5 @@ class MyHomePage extends StatelessWidget {
             )));
   }
 }
+
+ */
