f118c5d4cde1e7e97692fd307890be02163fb0c9
mdmanik74 <manikmhamud74@outlook.com>
2023-05-07 12:44:13 +0600

    flutter slider text size

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SliderApp());
+
+class SliderApp extends StatelessWidget {
+  const SliderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: const Color(0xff6750a4),
+        useMaterial3: true,
+      ),
+      home: const SliderExample(),
+    );
+  }
+}
+
+class SliderExample extends StatefulWidget {
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> {
+  double _currentSliderValue = 10.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Slider')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('তোমার ডাকনাম কি???',style: TextStyle(fontSize: _currentSliderValue),),
+          Slider(
+            value: _currentSliderValue,
+            min: 10,
+            max: 50,
+            divisions: 10,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import 'package:flutter/material.dart';
