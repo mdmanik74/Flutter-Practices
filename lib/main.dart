bf3b4f3dbefad23680392a2d42e653309baad761
mdmanik74 <manikmhamud74@outlook.com>
2023-05-09 12:41:31 +0600

    flutter create a checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,68 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Create a Checkbox',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Create a Checkbox'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _checkbox = false;
+  bool _checkboxListTile = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create a Checkbox'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Checkbox(
+                  value: _checkbox,
+                  onChanged: (value) {
+                    setState(() {
+                      _checkbox = !_checkbox;
+                    });
+                  },
+                ),
+                Text('I am true now'),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -55,9 +118,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     );
   }
 }
-
-
-
+*/
 /*
 import 'package:flutter/material.dart';
 void main() =>runApp(MyApp());
