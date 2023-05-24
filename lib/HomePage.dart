df13f69412cf87d7bb742a643098469040bac5fb
mdmanik74 <manikmhamud74@outlook.com>
2023-05-24 11:01:30 +0600

    Flutter Message chat desgin

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,104 +1,48 @@
 import 'package:flutter/material.dart';
-import 'HomePage.dart';
+class ChatModel {
+  final String avatarUrl;
+  final String name;
+  final String datetime;
+  final String message;
 
-void main() {
-  runApp(MyApp());
-}
+  ChatModel({required this.avatarUrl, required this.name, required this.datetime, required this.message});
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
-    );
-  }
-}
-
-class LoginDemo extends StatefulWidget {
-  @override
-  _LoginDemoState createState() => _LoginDemoState();
-}
-
-class _LoginDemoState extends State<LoginDemo> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: Container(
-                    width: 200,
-                    height: 150,
-                    /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                    child: Image.asset('asset/images/flutter-logo.png')),
-              ),
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            FlatButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (_) => HomePage()));
-                },
-                child: Text(
-                  'Login',style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 130,
-            ),
-            Text('New User? Create Account')
-          ],
-        ),
-      ),
-    );
-  }
-
-  FlatButton({required Null Function() onPressed, required Text child}) {}
+  static final List<ChatModel> dummyData = [
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    ChatModel(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,94 @@
 
 import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('fluttermaster.com - Message Chat'),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: ChatModel.dummyData.length,
+          itemBuilder: (context, index) {
+            ChatModel _model = ChatModel.dummyData[index];
+            return Column(
+              children: <Widget>[
+                Divider(
+                  height: 12.0,
+                ),
+                ListTile(
+                  leading: CircleAvatar(
+                    radius: 24.0,
+                    backgroundImage: NetworkImage(_model.avatarUrl),
+                  ),
+                  title: Row(
+                    children: <Widget>[
+                      Text(_model.name),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      Text(
+                        _model.datetime,
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                    ],
+                  ),
+                  subtitle: Text(_model.message),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 14.0,
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -90,7 +179,7 @@ Widget build(BuildContext context){
     ));
   }
 }
-
+*/
 
 /*
 import 'package:flutter/cupertino.dart';
