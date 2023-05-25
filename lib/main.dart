78703d08c93b1c847162072b0e56cf022aa101f0
mdmanik74 <manikmhamud74@outlook.com>
2023-05-25 18:24:38 +0600

    flutter data table

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,70 @@
 import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  static const String _title = 'Flutter Tutorial';
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: _title,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const Steps(),
+        appBar: AppBar(title: Text('DataTable Sample'),),
+        body: DataTableExample(),
       ),
     );
   }
 }
 
-class Step {
-  Step(
-      this.title,
-      this.body,
-      [this.isExpanded = false]
-      );
-  String title;
-  String body;
-  bool isExpanded;
-}
-
-Future<List<Step>> getSteps() async {
-  var _items = [
-    Step('Step 0: Install Flutter', 'Install Flutter development tools according to the official documentation.'),
-    Step('Step 1: Create a project', 'Open your terminal, run `flutter create <project_name>` to create a new project.'),
-    Step('Step 2: Run the app', 'Change your terminal directory to the project directory, enter `flutter run`.'),
-  ];
-  return Future<List<Step>>.delayed(const Duration(seconds: 2), () => _items);
-}
-
-class Steps extends StatelessWidget {
-  const Steps({Key? key}) : super(key: key);
+class DataTableExample extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child: FutureBuilder<List<Step>>(
-            future: getSteps(),
-            builder: (BuildContext context, AsyncSnapshot<List<Step>> snapshot) {
-              if(snapshot.hasData) {
-                return StepList(steps: snapshot.data ?? []);
-              }
-              else {
-                return Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(16),
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              }
-            }
+  Widget build(BuildContext context){
+    return DataTable(columns: [
+      DataColumn(
+          label: Expanded(
+            child: Text('Name',style: TextStyle(fontStyle: FontStyle.italic),),
+          ),
+
+      ),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Age',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
         ),
       ),
-    );
-  }
-}
 
-class StepList extends StatefulWidget {
-  final List<Step> steps;
-  const StepList({Key? key, required this.steps}) : super(key: key);
-  @override
-  State<StepList> createState() => _StepListState(steps: steps);
-}
-
-class _StepListState extends State<StepList> {
-  final List<Step> _steps;
-  _StepListState({required List<Step> steps}) : _steps = steps;
-  @override
-  Widget build(BuildContext context) {
-    return ExpansionPanelList(
-      expansionCallback: (int index, bool isExpanded) {
-        setState(() {
-          _steps[index].isExpanded = !isExpanded;
-        });
-      },
-      children: _steps.map<ExpansionPanel>((Step step) {
-        return ExpansionPanel(
-          headerBuilder: (BuildContext context, bool isExpanded) {
-            return ListTile(
-              title: Text(step.title),
-            );
-          },
-          body: ListTile(
-            title: Text(step.body),
+      DataColumn(
+        label: Expanded(
+          child: Text(
+            'Roll',
+            style: TextStyle(fontStyle: FontStyle.italic),
           ),
-          isExpanded: step.isExpanded,
-        );
-      }).toList(),
+        ),
+      ),
+    ],
+        rows: const <DataRow>[
+        DataRow(
+        cells: <DataCell>[
+        DataCell(Text('Sarah')),
+    DataCell(Text('19')),
+    DataCell(Text('Student')),
+    ],
+    ),
+    DataRow(
+    cells: <DataCell>[
+    DataCell(Text('Janine')),
+    DataCell(Text('43')),
+    DataCell(Text('Professor')),
+    ],
+    ),
+    DataRow(
+    cells: <DataCell>[
+    DataCell(Text('William')),
+    DataCell(Text('27')),
+    DataCell(Text('Associate Professor')),
+    ],
+    ),
+    ],
     );
   }
 }
