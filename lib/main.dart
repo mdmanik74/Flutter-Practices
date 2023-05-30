3168753314034a380e92c2bdbb9090a49c8f579c
mdmanik74 <manikmhamud74@outlook.com>
2023-05-30 14:32:56 +0600

    flutter tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,68 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Title of Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return DefaultTabController(
+        length: 6,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: createTabBar(),
+            title: Text('Flutter TabBar Example'),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text("Car")),
+              Center(child: Text("Transit")),
+              Center(child: Text("Bike")),
+              Center(child: Text("Boat")),
+              Center(child: Text("Railway")),
+              Center(child: Text("Bus"))
+            ],
+          ),
+        )
+    );
+  }
+
+  TabBar createTabBar()  {
+    return TabBar(
+      tabs: [
+        Row (children: [Icon(Icons.directions_car), SizedBox(width:5), Text("Car")]),
+        Row (children: [Icon(Icons.directions_transit), SizedBox(width:5), Text("Transit")]),
+        Row (children: [Icon(Icons.directions_bike), SizedBox(width:5), Text("Bike")]),
+        Row (children: [Icon(Icons.directions_boat), SizedBox(width:5), Text("Boat")]),
+        Row (children: [Icon(Icons.directions_railway), SizedBox(width:5), Text("Railway")]),
+        Row (children: [Icon(Icons.directions_bus), SizedBox(width:5), Text("Bus")]),
+      ],
+      isScrollable: true,
+    );
+  }
+}
+/*
+import 'package:flutter/material.dart';
+
 void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -43,16 +106,7 @@ class MyApp extends StatelessWidget{
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
+*/
 /*
 import 'package:flutter/material.dart';
 
