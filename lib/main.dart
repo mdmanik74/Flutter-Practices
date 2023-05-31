7b95d7d5281affbe3ff6e57e266601eedc2bb43d
mdmanik74 <manikmhamud74@outlook.com>
2023-05-31 13:04:08 +0600

    flutter splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+void main(){
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Future<Widget> loadFromFuture() async {
+
+    // <fetch data from server. ex. login>
+
+    return Future.value(new AfterSplash());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        navigateAfterFuture: loadFromFuture(),
+        title: new Text('Welcome In SplashScreen',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+        image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Flutter Egypt"),
+        loaderColor: Colors.red
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome In SplashScreen Package"),
+          automaticallyImplyLeading: false
+      ),
+      body: new Center(
+        child: new Text("Done!",
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0
+          ),),
+
+      ),
+    );
+  }
+}
+/*
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -60,7 +122,7 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
+*/
 /*
 import 'package:flutter/material.dart';
 
