7b1033a4b34943fc8696fdefffb485fa8d9514b2
mdmanik74 <manikmhamud74@outlook.com>
2023-06-17 10:47:18 +0600

    flutter Center Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main()=>runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'o7planning.org',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme:  ThemeData(primarySwatch: Colors.cyan),
+      home: MyHomePage(),
     );
+
   }
 }
 
-class HomePage extends StatefulWidget {
-
+class MyHomePage extends StatelessWidget{
   @override
-  State<StatefulWidget> createState() {
-    return _HomePageState();
-  }
-}
-
-class _HomePageState extends State<HomePage> {
-
-  bool _working = false;
-
-  void startWorking() async {
-    this.setState(() {
-      this._working = true;
-    });
-  }
-
-  void finishWorking() {
-    this.setState(() {
-      this._working = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter CircularProgressIndicator Example'),
-      ),
+      appBar: AppBar(title: Text('Flutter Center Example'),),
       body: Center(
-          child:  Column (
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: CircularProgressIndicator(
-                    value: this._working? null: 1,
-                    backgroundColor: Colors.cyanAccent,
-                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
-                  ),
-                ),
-                ElevatedButton(
-                    child: Text("Start"),
-                    onPressed: this._working? null: () {
-                      this.startWorking();
-                    }
-                ),
-                ElevatedButton(
-                    child: Text("Finish"),
-                    onPressed:!this._working? null: () {
-                      this.finishWorking();
-                    }
-                )
-              ]
-          )
+        child:Icon(
+          Icons.place,
+          size: 128,
+          color: Colors.redAccent,
+        ),
       ),
     );
   }
