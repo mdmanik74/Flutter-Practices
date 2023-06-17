3ae5a1ef43019253e2d31c3ff176e629e1b90f21
mdmanik74 <manikmhamud74@outlook.com>
2023-06-17 10:54:04 +0600

    flutter Row Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'o7planning.org',
       debugShowCheckedModeBanner: false,
-      theme:  ThemeData(primarySwatch: Colors.cyan),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: MyHomePage(),
     );
-
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
+
+
   @override
-  Widget build(BuildContext context){
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+
+class MyHomePageState extends State<MyHomePage> {
+  List<Widget> _children = [];
+  int idx = 0;
+
+  @override
+  void initState()  {
+    super.initState();
+
+    this._children = [
+      ElevatedButton(
+          key: Key(this.idx.toString()),
+          child: Text("Btn " + idx.toString()),
+          onPressed: (){}
+      )
+    ];
+  }
+
+  void addChildHandler()  {
+    this.idx++;
+    this.setState(() {
+      var newChild = ElevatedButton(
+          key: Key(this.idx.toString()),
+          child: Text("Btn " + idx.toString()),
+          onPressed: (){}
+      );
+      this._children.add(newChild);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Create new List object:
+
+    this._children = this._children == null? [] : List.from(this._children);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Center Example'),),
-      body: Align(
-        alignment: Alignment.bottomRight,
-        heightFactor: 2.0,
-        child: ElevatedButton(
-          child: Text('Button'),
-          onPressed: (){},
-        ),
+      appBar: AppBar(
+          title: Text("Flutter Row Example")
+      ),
+      body: Center(
+          child: Row (
+              children:  this._children
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: ()  {
+            this.addChildHandler();
+          }
       ),
     );
   }
