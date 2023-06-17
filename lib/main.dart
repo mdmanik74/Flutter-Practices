1be40d07eba447c08198d139b16f5d0231a1a177
mdmanik74 <manikmhamud74@outlook.com>
2023-06-17 14:58:44 +0600

    Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,37 @@
 import 'package:flutter/material.dart';
-
-void main() =>runApp(MyApp());
+void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
-
   Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.orange),
-
-      home: MyhomePage(),
-    );
-  }
-}
-
-class MyhomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter Column Example'),),
-      body: Center(
-          child: Column (
-              children: [
-                ElevatedButton(child: Text("Button 1"), onPressed:(){}),
-                Icon(Icons.ac_unit, size: 48, color: Colors.blue),
-                ElevatedButton(
-                    child: Text("Button 2"),
-                    onPressed:(){},
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size.square(70))
-                    )
-                ),
-                ElevatedButton(child: Text("Very Long Button 3"), onPressed:(){}),
-              ]
-          )
-      ),
-    );
-  }
-}
-
-/*
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'o7planning.org',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(),
+     home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget{
 
 
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomePageState();
-  }
-}
-
-
-class MyHomePageState extends State<MyHomePage> {
-  List<Widget> _children = [];
-  int idx = 0;
 
   @override
-  void initState()  {
-    super.initState();
-
-    this._children = [
-      ElevatedButton(
-          key: Key(this.idx.toString()),
-          child: Text("Btn " + idx.toString()),
-          onPressed: (){}
-      )
-    ];
-  }
-
-  void addChildHandler()  {
-    this.idx++;
-    this.setState(() {
-      var newChild = ElevatedButton(
-          key: Key(this.idx.toString()),
-          child: Text("Btn " + idx.toString()),
-          onPressed: (){}
+  Widget build(BuildContext context){
+    return InkWell(
+      onTap: (){
+        print('OnTap');
+      },
+      child: Container(
+        height: 200,
+        width: 200,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle, color: Colors.blue),
+       child: Center(child: Text('Ok'),
+       ),
+       ),
       );
-      this._children.add(newChild);
-    });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // Create new List object:
-
-    this._children = this._children == null? [] : List.from(this._children);
-
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("Flutter Row Example")
-      ),
-      body: Center(
-          child: Row (
-              children:  this._children
-          )
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: ()  {
-            this.addChildHandler();
-          }
-      ),
-    );
   }
 }
-
- */
