2b549440b2cd1d6637cf105d0b4eaeff353b7214
mdmanik74 <manikmhamud74@outlook.com>
2023-06-24 16:05:58 +0600

    flutter Page Routing

---
+++ lib/Page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Page2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title of Page 2"),
+      ),
+      body: Center(
+        child: Text('Page 2'),
+      ),
+      backgroundColor: Colors.lightGreen[100],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,64 @@
 import 'package:flutter/material.dart';
+import 'Page2.dart';
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Page Routing',style: TextStyle(color: Colors.white),),),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(50),
+      child: ElevatedButton(
+        child: Text('Goooo'),
+        onPressed: (){
+          Navigator.of(context).push(_createRoute());
+        },
+      ),
+
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (BuildContext context, Animation<double> animation,//
+        Animation<double> secondaryAnimation) {
+      return Page2();
+    },
+    transitionsBuilder: (BuildContext context, Animation<double> animation, //
+        Animation<double> secondaryAnimation, Widget child) {
+      return child;
+    },
+  );
+}
+
+/*
+import 'package:flutter/material.dart';
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -31,13 +91,13 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
 body: Container(
-  
+
   padding: EdgeInsets.all(60),
   alignment: Alignment.center,
   child: Column(children: [
     ElevatedButton(onPressed: (){
       ScaffoldMessenger.of(context).showSnackBar(
-       SnackBar(content: Text('Thanks for you ! Click Here',style: TextStyle(fontSize: 30.0,color: Colors.red),),
+       SnackBar(content: Text('Thanks for you ! Click Here....',style: TextStyle(fontSize: 20.0,color: Colors.red),),
          backgroundColor: Colors.black,
          elevation: 16,
        ),
@@ -50,3 +110,5 @@ body: Container(
     );
   }
 }
+
+*/
