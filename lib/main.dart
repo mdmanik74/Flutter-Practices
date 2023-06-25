663c4ca960331fdca7de97d55f87af398d3575d3
mdmanik74 <manikmhamud74@outlook.com>
2023-06-25 19:08:35 +0600

    flutter checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
-void main()=>runApp(const MyApp());
+void main() =>runApp(MyApp());
 
 class MyApp extends StatelessWidget{
-  const MyApp({super.key});
-
   @override
-  Widget build(BuildContext context){
+  Widget build (BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xffC4DFCB),
-        appBar: AppBar(
-        leading: const Icon(Icons.menu,color: Colors.black,),
-        title: Text('Flutter Custom Bottom',style: TextStyle(color:Theme.of(context).primaryColor,fontSize: 25,fontWeight: FontWeight.w600),),
-        centerTitle: true,
-          backgroundColor: Colors.white,
-        ),
-        ),
+      home: const MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget{
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Checkbox Example',style: TextStyle(color: Colors.white),),
+      ),
+      body: const MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool checkBoxValue=false;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: CheckboxListTile(
+            title: const Text('This is the checkbox with label'),
+            value: checkBoxValue,
+            activeColor: Colors.green,
+            onChanged: (bool? newValue) {
+              setState(() {
+                checkBoxValue = newValue!;
+              });
+            }));
+  }
+}
