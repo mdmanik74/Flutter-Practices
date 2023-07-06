d0661f8e0cef7cdec29802969659f86989bca202
mdmanik74 <manikmhamud74@outlook.com>
2023-07-06 13:44:16 +0600

    flutter profile desgin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-void main() =>runApp(MyApp());
+void main() =>runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build (BuildContext context){
     return MaterialApp(
@@ -9,44 +11,67 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(),
+      home: const ProfileScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
-  const MyHomePage({super.key});
+class ProfileScreen extends StatelessWidget{
+  const ProfileScreen({super.key});
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Checkbox Example',style: TextStyle(color: Colors.white),),
+        title: const Text('Flutter Profile Example',style: TextStyle(color: Colors.white),),
       ),
-      body: const MyStatefulWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 20,
+            ),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/1.jpg'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+            ListTile(
+              title: Text('Name'),
+              subtitle: Text('Md. Manik'),
+              leading: Icon(Icons.person),
+              trailing: Icon(Icons.arrow_forward,color: Colors.grey,),
+              minVerticalPadding: 25.0,
+            ),
+            Divider(height: 0,),
+            ListTile(
+              title: Text('Phone'),
+              subtitle: Text('0111777777'),
+              leading: Icon(Icons.call),
+              trailing: Icon(Icons.arrow_forward,color: Colors.grey,),
+            ),
+            Divider(height: 0,),
+            ListTile(
+              title: Text('Address'),
+              subtitle: Text('Dhaka, Bangladesh'),
+              leading: Icon(Icons.location_city_rounded),
+              trailing: Icon(Icons.arrow_forward,color: Colors.grey,),
+            ),
+            Divider(height: 10,),
+            ListTile(
+              title: Text('Email'),
+              subtitle: Text('name@gmail.com'),
+              leading: Icon(Icons.mail),
+              trailing: Icon(Icons.arrow_forward,color: Colors.grey,),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
 
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  bool checkBoxValue=false;
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: CheckboxListTile(
-            title: const Text('This is the checkbox with label'),
-            value: checkBoxValue,
-            activeColor: Colors.green,
-            onChanged: (bool? newValue) {
-              setState(() {
-                checkBoxValue = newValue!;
-              });
-            }));
-  }
-}
