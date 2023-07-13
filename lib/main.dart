23f1c2e81ef850087f8977a9f14bd1765a002259
mdmanik74 <manikmhamud74@outlook.com>
2023-07-13 16:38:50 +0600

    login page degin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'HomeIcon.dart';
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,214 +8,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: Scaffold(
-        drawer: const NavigationDrawer(
-          children: [],
-        ),
-        appBar: AppBar(
-          title: const Text(
-            'Navigation Drawer Widget',
-          ),
-          backgroundColor: Colors.green,
-        ),
-        body: const MyHomePage(),
-      ),
+      home: ProfileScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class ProfileScreen extends StatelessWidget {
+  ProfileScreen({super.key});
+  final _formField = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onLongPress: () {
-              Navigator.of(context)
-                  .pushReplacementNamed(const HomeIcon() as String);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.favorite_border),
-            title: const Text('Home'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.workspaces),
-            title: const Text('Workflow'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.update),
-            title: const Text('Update'),
-            onTap: () {},
-          ),
-          const Divider(
-            color: Colors.black54,
-          ),
-          ListTile(
-            leading: const Icon(Icons.plumbing),
-            title: const Text('Plugin'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: const Icon(Icons.notification_add),
-            title: const Text('Notifaction'),
-            onTap: () {},
-          ),
-        ],
-      )),
-    );
-  }
-}
-
-
-
-
-/*
-import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Switch List Widget',
-          ),
-          backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Profile Example',
+          style: TextStyle(color: Colors.white),
         ),
-        body: const MyHomePage(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _valueN = false;
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.00),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.purple),
-          ),
-          child: SwitchListTile(
-            value: _valueN,
-            onChanged: (value) {
-              setState(() {
-                _valueN = value;
-              });
-            },
-            title: const Text(
-              'Plese Selected Yes/No Option',
-              style: TextStyle(
-                  fontFamily: 'Dancing_Script', fontStyle: FontStyle.normal),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+          child: Column(children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/6.jpg'),
+              radius: 50,
             ),
-            secondary: const Icon(Icons.switch_right),
-            activeColor: Colors.red,
-            inactiveThumbColor: Colors.black,
-            selected: false,
-          ),
+            const SizedBox(height: 50),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email)),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.visiblePassword,
+              controller: passController,
+              decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock)),
+            ),
+            const SizedBox(height: 60),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
       ),
     );
   }
 }
-
-*/
-
-/*
-import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Switch Button Widget',
-          ),
-          backgroundColor: Colors.green,
-        ),
-        body: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _valueN = false;
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.00),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.purple),
-          ),
-          child: Switch(
-            value: _valueN,
-            onChanged: (value) {
-              setState(() {
-                _valueN = value;
-              });
-            },
-            activeColor: Colors.red,
-            inactiveThumbColor: Colors.black,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-*/
