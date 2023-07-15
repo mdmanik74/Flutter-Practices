695f0b747b1b4715458b6158405738a64915f6cc
mdmanik74 <manikmhamud74@outlook.com>
2023-07-15 12:31:31 +0600

    page route

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,17 +1,18 @@
 
-/*
 import 'package:flutter/material.dart';
 class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Title of Page 2"),
+        title: const Text("Title of Page 2"),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Page 2'),
       ),
       backgroundColor: Colors.lightGreen[100],
     );
   }
 }
-*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,48 @@
 import 'package:flutter/material.dart';
+import 'Page2.dart';
 
-void main() {
-  runApp(const RunMyApp());
-}
+void main() => runApp(const MyApp());
 
-class RunMyApp extends StatefulWidget {
-  const RunMyApp({super.key});
-
-  @override
-  State<RunMyApp> createState() => _RunMyAppState();
-}
-
-class _RunMyAppState extends State<RunMyApp> {
-  ThemeMode _themeMode = ThemeMode.system;
-  void changeTheme(ThemeMode themeMode) {
-    setState(() {
-      _themeMode = themeMode;
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-
-      // standard dark theme
-      darkTheme: ThemeData.dark(),
-      themeMode: _themeMode,
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Device Controlled theme Mode'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Choose your theme:',
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  // Change theme & rebuild to
-                  // show it using these buttons
-                  ElevatedButton(
-                      onPressed: () {
-                        changeTheme(ThemeMode.light);
-                      },
-                      child: const Text('Light theme')),
-                  ElevatedButton(
-                      onPressed: () {
-                        changeTheme(ThemeMode.dark);
-                      },
-                      child: const Text('Dark theme')),
-                ],
-              ),
-            ],
-          ),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
       ),
+      home: ProfileScreen(),
     );
   }
 }
 
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Profile Example',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        child: Center(
+            child: ElevatedButton(
+          child: const Text('Please Click Here'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Page2()));
+          },
+        )),
+      ),
+    );
+  }
+}
 
 
 /*
