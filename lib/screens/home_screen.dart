c5b4410978380bd52da1a4d00763a30eac75fadc
mdmanik74 <manikmhamud74@outlook.com>
2023-07-25 19:48:02 +0600

    home screen course

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -57,106 +57,164 @@ class _HomeScreenState extends State<HomeScreen> {
       size: 30,
     ),
   ];
+  List imgList = [
+    'Flutter',
+    'React Native',
+    'Python',
+    'C#',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            padding:
-                const EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
-            decoration: const BoxDecoration(
-              color: kblue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Icon(
-                    Icons.dashboard,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                  Icon(
-                    Icons.notification_add,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 3, bottom: 15),
-                child: Text(
-                  'Hi Programmer',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 1,
-                      wordSpacing: 2,
-                      color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 15, left: 15, right: 15, bottom: 10),
+              decoration: const BoxDecoration(
+                color: kblue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 5, bottom: 20),
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10)),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Search Here',
-                      hintStyle: TextStyle(
-                        color: Colors.black.withOpacity(0.5),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Icon(
+                          Icons.dashboard,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        Icon(
+                          Icons.notification_add,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 3, bottom: 15),
+                      child: Text(
+                        'Hi Programmer',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1,
+                            wordSpacing: 2,
+                            color: Colors.white),
                       ),
-                      prefixIcon: const Icon(
-                        Icons.search,
-                        size: 25,
-                      )),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 5, bottom: 20),
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search Here',
+                            hintStyle: TextStyle(
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                            prefixIcon: const Icon(
+                              Icons.search,
+                              size: 25,
+                            )),
+                      ),
+                    )
+                  ]),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+              child: Column(children: [
+                GridView.builder(
+                  itemCount: catNames.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (BuildContext context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: catColors[index],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: catIcons[index],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          catNames[index],
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(0.7)),
+                        )
+                      ],
+                    );
+                  },
                 ),
-              )
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
-            child: Column(children: [
-              GridView.builder(
-                itemCount: catNames.length,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1.1,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Courses',
+                      style:
+                          TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'See All',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF674AEF)),
+                    ),
+                  ],
                 ),
-                itemBuilder: (BuildContext context, index) {
-                  return Column(
-                    children: [
-                      Container(
-                        height: 60,
-                        width: 60,
+                const SizedBox(height: 10),
+                GridView.builder(
+                  itemCount: imgList.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 4,
+                      mainAxisExtent: 10,
+                      crossAxisSpacing: 10),
+                  itemBuilder: (BuildContext context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 10),
                         decoration: BoxDecoration(
-                          color: catColors[index],
-                          shape: BoxShape.circle,
+                          borderRadius: BorderRadius.circular(20),
+                          color: const Color.fromARGB(255, 243, 219, 5),
                         ),
-                        child: Center(
-                          child: catIcons[index],
-                        ),
-                      )
-                    ],
-                  );
-                },
-              )
-            ]),
-          )
-        ],
+                      ),
+                    );
+                  },
+                ),
+              ]),
+            ),
+          ],
+        ),
       ),
     );
   }
